9a95e285d97761302422fed02c0409d5f5a5cde6
Hedayat Farahi <hedayatfarahi8@gmail.com>
2022-03-05 19:56:10 +0100

    add the admin login

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./Admin.css"
 import Footer from './Footer'
 import logo from "./images/logo.png"
 
 const Admin = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if(username === "azadi" && password === "azadi@321$"){
+      window.location.href = "http://localhost:3000";
+    } else {
+      alert("Invalid username or password");
+    }
+  }
+
+
   return (
     <div className='admin'>
         <div className="admin__inputs">
             <Link to={"/"}><img src={logo} alt="" /></Link>
             <div className="admin__input">
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" />
+                <input value={username} onChange={(e)=> setUsername(e.target.value)} type="text" id="username" />
             </div>
             <div className="admin__input">
                 <label htmlFor="username">Password</label>
-                <input type="password" id="username" />
+                <input value={password} onChange={(e)=> setPassword(e.target.value)} type="password" id="username" />
             </div>
-            <button className='admin__button'>Login</button>
+            <button onClick={handleLogin} className='admin__button'>Login</button>
         </div>
     </div>
   )
